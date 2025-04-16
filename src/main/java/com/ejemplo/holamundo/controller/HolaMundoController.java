8c149197ab1506f1e92aa928481e944b4e045760
alejimenezp-profe <ale.jimenezp@profesor.duoc.cl>
2025-04-16 13:19:39 -0400

    Comit inicial

---
+++ src/main/java/com/ejemplo/holamundo/controller/HolaMundoController.java
@@ -0,0 +1,25 @@
+package com.ejemplo.holamundo.controller;
+
+import jakarta.websocket.server.PathParam;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/bla")
+public class HolaMundoController {
+
+    @GetMapping("/hola")
+    public String holaMundo(){
+        System.out.println("prueba de log");
+        return "Hola Mundo!";
+    }
+
+    @GetMapping("/saluda/{nombre_saludo}")
+    public String saludaNombre(@PathVariable("nombre_saludo") String nombre_saludo){
+        return "hola que tal "+nombre_saludo;
+    }
+
+    @GetMapping("/saluda-query")
+    public String saludaQueryString(@RequestParam("nombre") String nombre){
+        return "hola otro nombre "+nombre;
+    }
+}
